db23d4347297140e0e1aa4fec6bec22f0b49a97f
Janosch Woschitz <janosch.woschitz@gmail.com>
2014-04-30 00:10:05 +0200

    added TypeCheckerArgumentTypeTest

---
+++ test/rg/typewriter/TypeCheckerArgumentTypeTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace rg\typewriter;
+
+class TypeCheckerArgumentTypeTest extends \PHPUnit_Framework_TestCase {
+
+    public function testValidateScalarArgumentWithName() {
+        $this->markTestIncomplete();
+    }
+}
